e171385c49fd2b0be96395e2efcdf828c3c653a1
niburo <ty.lmmh10@gmail.com>
2020-11-14 23:56:30 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,14 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new]
+  before_action :set_item,only: [:edit,:show,:update]
+
   def index
     @item = Item.includes(:user).order("created_at DESC")
   end
 
   def new
     @item = Item.new
-  end
-
+   end
 
   def create
     @item = Item.new(item_params)
@@ -19,13 +20,28 @@ class ItemsController < ApplicationController
   end
 
   def show
-     @item = Item.find(params[:id])
   end
   
+  def edit
+   redirect_to new_user_session_path unless user_signed_in? && current_user.id == @item.user_id
+  end
+
+  def update
+    if @item.update(item_params)
+       redirect_to item_path(@item.id)
+    else
+       render :edit
+    end
+  end
+
 
   private
    def item_params
      params.require(:item).permit(:name, :image, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
+   end
+
+   def set_item
+      @item = Item.find(params[:id])
    end
 
 end
