a6fade37642921ef72524b765392eab4897dd703
niburo <ty.lmmh10@gmail.com>
2020-11-18 15:40:22 +0900

    商品購入機能の修正２

--- app/models/addresse_purchase.rb
+++ app/models/addresse_purchase.rb
@@ -1,7 +1,7 @@
 class AddressePurchase < ApplicationRecord
 
     include ActiveModel::Model
-    attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id
+    attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number, :user_id, :item_id
     attr_accessor :token
     with_options presence: true do
       validates :postal_code,  format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
